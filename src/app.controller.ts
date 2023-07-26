b2cf94c0cbcd637523ffe7e56ca324d4318ae01a
worashf <uppert83@gmail.com>
2023-07-26 22:16:36 +0300

    Finish crud

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,58 @@
-import { Controller, Delete, Get , Post, Put, Param} from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param, Body, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
-import  {ReportType, data} from  "./data"
+import { ReportType, data } from "./data"
+import { v4 as uuid } from "uuid"
+
 
 @Controller("report/:type")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getAllReports(@Param("type") type:string){
-    let reportType  = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
-    return  data.report.filter(report => report.type ==reportType)
+  getAllReports(@Param("type") type: string) {
+    let reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    return data.report.filter(report => report.type == reportType)
   }
   @Get(':id')
-  getReportByID(){
-  return  {}
+  getReportByID(@Param("type") type: string, @Param("id") id: string) {
+    let reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    return data.report.filter(report => report.type === reportType).find(repo => repo.id === id)
   }
   @Post()
-  createReport(){
-     return "Created"
+  createReport(@Body() { source, amount }: { source: string, amount: number },
+    @Param("type") type: string) {
+    let reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: reportType
+    }
+    data.report.push(newReport)
+    return newReport
   }
   @Put(":id")
-  updateReport(){
-    return "Updated"
-  }
+  updateReport(@Param("type") type :string,
+  @Param("id") id :string, 
+  @Body() reportBody: {source: string, amount:number}) {
+    let reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+const reportToUpdate  = data.report.filter(report => report.type === reportType).find(repo => repo.id === id)
+if(!reportToUpdate) return
+let reportIndex = data.report.findIndex(report => report.id  === reportToUpdate.id)
+data.report[reportIndex] ={
+  ...data.report[reportIndex],
+  ...reportBody
+}
+ return  data.report[reportIndex]
+}
   @Delete(":id")
-  deleteReport(){
-    return "Deleted"
+  @HttpCode(204)
+  deleteReport(@Param("id") id:string) {
+    let reportIndex  = data.report.findIndex(report => report.id  === id)
+    if(reportIndex ===  -1) return
+    data.report.splice(reportIndex, 1)
+    return;
   }
 }
